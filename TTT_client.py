b76136f2d7a329b2774c89c75d4a658f15d69136
Ragzzzzze <126308447+Ragzzzzze@users.noreply.github.com>
2024-09-23 20:00:07 +1000

    Update TTT_client.py
    Added the ability to play step by step
    - new way of creating window
    - current turn is fixed

--- TTT_client.py
+++ TTT_client.py
@@ -3,11 +3,8 @@ import tic_tac_toe
 import threading
 import socket
 
-icon = pygame.image.load("models//icon.png")
-
-surface = pygame.display.set_mode((tic_tac_toe.WIDTH, tic_tac_toe.HEIGHT))
-pygame.display.set_caption("Client")
-pygame.display.set_icon(icon)
+grid = tic_tac_toe.Grid()
+surface = grid.set_my_window("client")
 
 def create_thread(target):
     thread_gen = threading.Thread(target=target, daemon=True)
@@ -18,20 +15,22 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("127.0.0.1", 12345))
 
 def received_data():
+    global turn
     while True:
         data = client.recv(1024).decode()
         data = data.split('.')
         x, y = int(data[0]), int(data[1])
         #x, y - coordinates of opponent's move
-        #turn_status - current turn status
-
+        turn = True
+        if grid.get_cell_value(x, y) == 0:
+            grid.set_cell_value(x, y, opponent)
         print(data)
 
 
 create_thread(received_data)
 
+turn = False
 running = True
-grid = tic_tac_toe.Grid()
 opponent = "X"
 player = "O"
 
@@ -40,15 +39,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and not grid.game_over and turn:
             pos = pygame.mouse.get_pos()
             x_pos, y_pos = pos[0] // int(tic_tac_toe.WIDTH / 3), pos[1] // int(tic_tac_toe.HEIGHT / 3)
             grid.get_mouse(x_pos, y_pos, player)
 
             data = '{}.{}'.format(x_pos, y_pos).encode()
             client.send(data)
+            turn = False
 
 
     surface.fill(tic_tac_toe.BG_COLOR)
     grid.draw(surface)
     pygame.display.update()
+
