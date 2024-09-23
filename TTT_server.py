ade9d35b3b1ed2a4bd6c9621e802178dc3338c27
Ragzzzzze <126308447+Ragzzzzze@users.noreply.github.com>
2024-09-23 20:01:56 +1000

    Update TTT_server.py
    It's like update TTT_client.py

--- TTT_server.py
+++ TTT_server.py
@@ -1,16 +1,14 @@
-import pygame
 import tic_tac_toe
+import pygame
 import threading
 import socket
 
-icon = pygame.image.load("models//icon.png")
-
-surface = pygame.display.set_mode((tic_tac_toe.WIDTH, tic_tac_toe.HEIGHT))
-pygame.display.set_caption("Server")
-pygame.display.set_icon(icon)
-
 connection_success = False
 user, address = None, None
+
+grid = tic_tac_toe.Grid()
+surface = grid.set_my_window("Server")
+
 
 def create_thread(target):
     thread_gen = threading.Thread(target=target, daemon=True)
@@ -22,6 +20,19 @@ server.bind(("127.0.0.1", 12345))
 server.listen(1)
 
 
+def received_data():
+    global turn
+    while True:
+        data = user.recv(1024).decode()
+        data = data.split('.')
+        x, y = int(data[0]), int(data[1])
+        #x, y - coordinates of opponent's move
+        #turn_status - current turn status
+        turn = True
+        if grid.get_cell_value(x, y) == 0:
+            grid.set_cell_value(x, y, opponent)
+        print(data)
+
 def connection():
     global user, address, connection_success
     user, address = server.accept()
@@ -29,37 +40,28 @@ def connection():
     received_data()
 
 
-def received_data():
-    while True:
-        data = user.recv(1024).decode()
-        data = data.split('.')
-        x, y = int(data[0]), int(data[1])
-        #x, y - coordinates of opponent's move
-        #turn_status - current turn status
-
-        print(data)
-
+turn = True
+running = True
+player = "X"
+opponent = "O"
 
 create_thread(connection)
 
-running = True
-grid = tic_tac_toe.Grid()
-opponent = "O"
-player = "X"
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN and connection_success:
+        if event.type == pygame.MOUSEBUTTONDOWN and connection_success and turn:
             pos = pygame.mouse.get_pos()
             x_pos, y_pos = pos[0] // int(tic_tac_toe.WIDTH / 3), pos[1] // int(tic_tac_toe.HEIGHT / 3)
             grid.get_mouse(x_pos, y_pos, player)
 
-            data = '{}.{}'.format(x_pos, y_pos).encode()
-            user.send(data)
 
+            data = '{}.{}'.format(x_pos, y_pos)
+            user.send(data.encode())
+            turn = False
 
 
     surface.fill(tic_tac_toe.BG_COLOR)
