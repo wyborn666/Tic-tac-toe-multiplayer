6adfd4bbd678058af376efdbc1a017272ff7c3c2
Ragzzzzze <126308447+Ragzzzzze@users.noreply.github.com>
2024-10-01 20:01:33 +1000

    Add multicast and result board

--- TTT_client.py
+++ TTT_client.py
@@ -3,14 +3,22 @@ import pygame
 import threading
 import socket
 import re
+import struct
+
+MCAST_GRP = '224.0.0.1'
+MCAST_PORT = 5007
+ANY = "0.0.0.0"
  
 
 class Client:
     def __init__(self):
         self.turn = False
         self.running = True
+        self.connection_success = False
         self.player = "O"
         self.opponent = "X"
+        self.available_servers = {}
+
 
         self.grid = tic_tac_toe.Grid()
         self.surface = self.grid.set_my_window("client")
@@ -18,6 +26,14 @@ class Client:
     def create_thread(self, target):
         thread_gen = threading.Thread(target=target, daemon=True)
         thread_gen.start()
+
+    def listen_for_servers(self):
+        while True:
+            data, addr = self.mltClient.recvfrom(1024)
+            server_info = data.decode()
+            if server_info not in self.available_servers:
+                self.available_servers[server_info] = addr
+                print(f"Found server: {server_info} at {addr}")
 
     def received_data(self):
         while True:
@@ -27,49 +43,102 @@ class Client:
                 self.grid.reset()
                 self.turn = False
 
-            elif re.match(r'\d.\d$', data):
+            elif re.match(r'\d.\d.\d$', data):
                 data = data.split('.')
-                x, y = int(data[0]), int(data[1])
+                x, y, condition = int(data[0]), int(data[1]), int(data[2])
                 self.grid.set_cell_value(x, y, self.opponent)
+                if condition:
+                    self.grid.score_wins_x += 1
                 self.turn = True
+    '''
+    def main_menu(self):
+        self.mltClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.mltClient.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.mltClient.bind((ANY, MCAST_PORT))
 
-    
-    def game(self):
+        mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+        self.mltClient.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+        print("Searching for servers...")
+
+        self.create_thread(self.listen_for_servers)
+
+        print("\nAvailable servers:")
+        for i, server in enumerate(self.available_servers):
+            print(f"{i + 1}. {server}")
+
+        choice = int(input("Select server number to connect to: ")) - 1
+        selected_server = list(self.available_servers.keys())[choice]
+        server_ip, server_port = selected_server.split(':')
+        server_port = int(server_port)
 
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect(("127.0.0.1", 12345))
+        self.client.connect((server_ip, server_port))
+        print(f"Connected to server {server_ip}:{server_port}")
         
         self.create_thread(self.received_data)
+    '''
+    
+    def game(self):
+        self.mltClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.mltClient.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.mltClient.bind((ANY, MCAST_PORT))
 
+        mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+        self.mltClient.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+        print("Searching for servers...")
+
+        self.create_thread(self.listen_for_servers)
+
+        print("\nAvailable servers:")
+        for i, server in enumerate(self.available_servers):
+            print(f"{i + 1}. {server}")
+
+        choice = int(input("Select server number to connect to: ")) - 1
+        selected_server = list(self.available_servers.keys())[choice]
+        server_ip, server_port = selected_server.split(':')
+        server_port = int(server_port)
+
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((server_ip, server_port))
+        print(f"Connected to server {server_ip}:{server_port}")
+        
+        self.create_thread(self.received_data)
                 
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
-                if event.type == pygame.MOUSEBUTTONDOWN and not self.grid.game_over and self.turn:
+                if event.type == pygame.MOUSEBUTTONDOWN and self.turn and not self.grid.game_over:
                     pos = pygame.mouse.get_pos()
                     x_pos, y_pos = pos[0] // int(tic_tac_toe.WIDTH / 3), pos[1] // int(tic_tac_toe.HEIGHT / 3)
                     self.grid.get_mouse(x_pos, y_pos, self.player)
 
                     if self.grid.switch:
-                        data = '{}.{}'.format(x_pos, y_pos).encode()
+                        if self.grid.win_case(self.player):
+                            self.grid.score_wins_o += 1
+
+                        data = '{}.{}.{}'.format(x_pos, y_pos, self.grid.condition).encode()
                         self.client.send(data)
                         self.turn = False
-                
-                if self.grid.win_case(self.player):
-                    data = "new"
-                    self.user.send(data.encode())
-                    print(f"Player {self.player} wins!")
-                    self.grid.reset()
-                    self.turn = False
 
-
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE and self.grid.game_over:
+                        data = "new"
+                        self.client.send(data.encode())
+                        self.grid.reset()
+            
             self.surface.fill(tic_tac_toe.BG_COLOR)
             self.grid.draw(self.surface)
+
+            if self.grid.game_over or self.grid.win_case(self.opponent) or self.grid.draw_case():
+                self.grid.update_score_and_display(self.surface,self.player)
+
             pygame.display.update()
 
-
+pygame.init()
 client = Client()
 client.game()
 
--- TTT_server.py
+++ TTT_server.py
@@ -3,12 +3,20 @@ import pygame
 import threading
 import socket
 import re
+import struct
+import time
+import sys
 
+MCAST_GRP = '224.0.0.1'
+MCAST_PORT = 5007
+SERVER_PORT = 1234
+SERVER_IP = socket.gethostbyname(socket.getfqdn())
 
 class Server:
     def __init__(self):
-        self.connection_success = False
         self.user, self.address = None, None
+        self.connection_success = False
+        self.reset_var = False
         self.turn = True
         self.running = True
         self.player = "X"
@@ -21,8 +29,18 @@ class Server:
         thread_gen = threading.Thread(target=target, daemon=True)
         thread_gen.start()
 
+    def announce_server(self):
+        mltSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        mltSocket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, struct.pack('b', 1))
+        while True:
+            message = f"{SERVER_IP}:{SERVER_PORT}"
+            mltSocket.sendto(message.encode(), (MCAST_GRP, MCAST_PORT))
+            print(f"Announced server: {message}")
+            time.sleep(5)
+
     def connection(self):
         self.user, self.address = self.server.accept()
+        print(f"Connected to {self.address}")
         self.connection_success = True
         self.received_data()
 
@@ -34,24 +52,48 @@ class Server:
                 self.grid.reset()
                 self.turn = True
 
-            elif re.match(r'\d.\d$', data):
+            elif re.match(r'\d.\d.\d$', data):
                 data = data.split('.')
-                x, y = int(data[0]), int(data[1])
+                x, y, condition = int(data[0]), int(data[1]), int(data[2])
                 self.grid.set_cell_value(x, y, self.opponent)
+                if condition:
+                    self.grid.score_wins_o += 1
                 self.turn = True
 
-    def game(self):
+    def main_menu(self):
+        self.create_thread(self.announce_server)
+
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind(("127.0.0.1", 12345))
+        self.server.bind((SERVER_IP, SERVER_PORT))
         self.server.listen(1)
         
         self.create_thread(self.connection)
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.surface.fill(tic_tac_toe.MENU_COLOR)
+            pygame.display.update()
+            if self.connection_success:
+                running = False
+                self.game()
+
+
+
+
+    def game(self):
+
+        #Add function that will draw main menu. MLC_thread will create in that function.
+        self.create_thread(self.announce_server)
 
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-
+                
                 if event.type == pygame.MOUSEBUTTONDOWN and self.connection_success and self.turn and not self.grid.game_over:
         
                     pos = pygame.mouse.get_pos()
@@ -59,20 +101,30 @@ class Server:
                     self.grid.get_mouse(x_pos, y_pos, self.player)
 
                     if self.grid.switch:
-                        data = '{}.{}'.format(x_pos, y_pos)
+                        if self.grid.win_case(self.player):
+                            self.grid.score_wins_x += 1
+
+                        data = '{}.{}.{}'.format(x_pos, y_pos, self.grid.condition)
                         self.user.send(data.encode())
                         self.turn = False
-                
-                if self.grid.win_case(self.player):
-                    data = "new"
-                    self.user.send(data.encode())
-                    print(f"Player {self.player} wins!")
-                    self.grid.reset()
-                    self.turn = True
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE and self.grid.game_over:
+                        data = "new"
+                        self.user.send(data.encode())
+                        self.grid.reset()
+                        self.turn = True
             
             self.surface.fill(tic_tac_toe.BG_COLOR)
             self.grid.draw(self.surface)
+
+            if self.grid.game_over or self.grid.win_case(self.opponent) or self.grid.draw_case():
+                self.grid.update_score_and_display(self.surface,self.player)
+
             pygame.display.update()
 
+
+
+pygame.init()
 serv = Server()
-serv.game()
+serv.main_menu()
